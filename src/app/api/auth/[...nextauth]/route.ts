a4939df33e756e1cbdf17c0271540ea824ba7131
nnachi kingsley <chimakingsley216@gmail.com>
2025-02-20 23:59:34 +0100

    New changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,36 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
+// import NextAuth, { AuthOptions } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const res = await fetch("YOUR_BACKEND_API/auth/login", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(credentials),
-        });
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     }),
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: { label: "Email", type: "email" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         const res = await fetch("YOUR_BACKEND_API/auth/login", {
+//           method: "POST",
+//           headers: { "Content-Type": "application/json" },
+//           body: JSON.stringify(credentials),
+//         });
 
-        const user = await res.json();
-        if (res.ok && user) return user;
-        return null;
-      },
-    }),
-  ],
-  pages: { signIn: "/signin" },
-  session: { strategy: "jwt" },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+//         const user = await res.json();
+//         if (res.ok && user) return user;
+//         return null;
+//       },
+//     }),
+//   ],
+//   pages: { signIn: "/signin" },
+//   session: { strategy: "jwt" },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
 
-const handler = NextAuth(authOptions as AuthOptions);
-export { handler as GET, handler as POST };
+// const handler = NextAuth(authOptions as AuthOptions);
+// export { handler as GET, handler as POST };
